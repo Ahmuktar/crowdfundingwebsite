0ee81ca5266fdc16308ba1ca2c9c4460e3b07846
Ahmad Muktar <ahmuktar203@gmail.com>
2025-01-18 11:18:40 +0100

    fix route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import  ThemeContext  from "./context/ThemeContext"; 
+import ThemeContext from "./context/ThemeContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react/demo">
+    <BrowserRouter basename="/">
       <ThemeContext>
         <App />
       </ThemeContext>
